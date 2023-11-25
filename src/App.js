cf826adbe889f5a5295b77d0f8313c84405c9257
RaviDobariya <124256250+rajvirDobariya@users.noreply.github.com>
2023-11-25 12:47:42 +0530

    Complete React Js with Project tutorial in hindi- Durgesh 01:09

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button } from 'reactstrap';
+import { ToastContainer, toast } from 'react-toastify';
+
 
 function App() {
+
+  const notify = () => {
+    toast.success("Wow so easy !")
+ 
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>FirstReact Button</h1>
+      <div>
+        <Button onClick={notify}>Notify !</Button>
+        <ToastContainer position="top-center" />
+      </div>
+    </>
   );
 }
 
